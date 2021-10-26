591edefeefec6bf2a789411dae1b03df0fbf3418
Alexander Manley <alexander-manley@users.noreply.github.com>
2021-10-26 17:10:33 -0400

    Update home.py

--- routes/home.py
+++ routes/home.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 app_home = APIRouter()
 
 
-@app_home.get('/', tags=["Intro"])
+@app_home.get('/', tags=["home"])
 async def home():
     return {"message": "/"}
 
 
-@app_home.get('/route1', tags=["Intro"])
+@app_home.get('/route1', tags=["root"])
 async def ml1():
     return {"message": "/ml1"}
